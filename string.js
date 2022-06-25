ad47945fb4eb4eaba5400b7e094cd9a83fe8f0c1
omerfarukyapici <omerfarukyapici20@gmail.com>
2022-06-25 20:40:46 +0300

    Added concat() and repeat() string methods

--- string.js
+++ string.js
@@ -1,8 +1,23 @@
+//🔔🔔👉👉👇👇👇📌📌📌
+
+//toLowerCase(), toUpperCase()
 const myName = 'OmerFarukYapici';
-console.log(myName.toLowerCase());
-console.log(myName.toUpperCase());
+//console.log(myName.toLowerCase());
+//console.log(myName.toUpperCase());
 /* 
 output👇
 omerfarukyapici 
 OMERFARUKYAPICI  
+*/
+
+//concat(), repeat()
+const str1 = "A";
+const str2 = 'B';
+const str3 = `C`;
+
+const myLetters = str1.concat(str2, str3, " ").repeat(3);
+console.log(myLetters);
+/* 
+output👇
+ABC ABC ABC
 */
